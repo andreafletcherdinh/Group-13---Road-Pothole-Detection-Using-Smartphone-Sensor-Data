0c3e2c9f3578507f2ecd3547550ef1620f9b714e
Đinh Huỳnh Gia Bảo <giabaoctg@gmail.com>
2024-12-27 15:33:17 +0700

    feat(dashboard): Add comment about how this interface works

--- app/src/main/java/com/example/road_pothole_detection_13/app_ui/dashboard/ApiService.java
+++ app/src/main/java/com/example/road_pothole_detection_13/app_ui/dashboard/ApiService.java
@@ -1,9 +1,17 @@
+// File: ApiService.java
+/* TODO: Khởi tạo API Endpoints
+ * Retrofit yêu cầu interface để generate HTTP client
+ * 
+*/
+
+
 package com.example.road_pothole_detection_13.app_ui.dashboard;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
+// Định nghĩa một API endpoint để lấy dữ liệu về các ổ gà thông qua một yêu cầu HTTP GET.
 public interface ApiService {
-    @GET("http://diddysfreakoffparty.online:3000/api/map/potholes")
+    @GET("YOUR_API_ENDPOINT")
     Call<PotholeResponse> getPotholes();
 }
